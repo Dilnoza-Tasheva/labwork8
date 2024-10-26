038fc5723457c750debfc0622f726b4a4f2433c4
Dilnoza Tasheva <dilnozatasheva@gmail.com>
2024-10-26 08:56:31 +0200

    Added categories as a list (via MUI component) to the Home page. Added Navlink components to the list items - will need to ensure they work properly. Move on to the form

--- src/Containers/Home/Home.tsx
+++ src/Containers/Home/Home.tsx
@@ -1,10 +1,52 @@
+import { Box, List, ListItem, ListItemButton, ListItemText } from '@mui/material';
+import Grid from '@mui/material/Grid2';
+import { NavLink } from 'react-router-dom';
 
 
 const Home = () => {
   return (
-    <div>
-      Home
-    </div>
+    <>
+      <Grid container spacing={2}>
+        <Box sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+          <nav aria-label="main mailbox folders">
+            <List>
+              <ListItem disablePadding>
+                <ListItemButton component={NavLink} to="/">
+                  <ListItemText primary="All" />
+                </ListItemButton>
+              </ListItem>
+              <ListItem disablePadding>
+                <ListItemButton component={NavLink} to="/">
+                  <ListItemText primary="Star Wars" />
+                </ListItemButton>
+              </ListItem>
+              <ListItem disablePadding>
+                <ListItemButton component={NavLink} to="/">
+                  <ListItemText primary="Famous people" />
+                </ListItemButton>
+              </ListItem>
+              <ListItem disablePadding>
+                <ListItemButton component={NavLink} to="/">
+                  <ListItemText primary="Saying" />
+                </ListItemButton>
+              </ListItem>
+              <ListItem disablePadding>
+                <ListItemButton component={NavLink} to="/">
+                  <ListItemText primary="Humor" />
+                </ListItemButton>
+              </ListItem>
+              <ListItem disablePadding>
+                <ListItemButton component={NavLink} to="/">
+                  <ListItemText primary="Motivational" />
+                </ListItemButton>
+              </ListItem>
+            </List>
+          </nav>
+        </Box>
+        Home
+      </Grid>
+
+    </>
   );
 };
 
